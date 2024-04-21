133f12e5ab5e39143307320c79e7282b560d4521
pietrociatta <pietro.ciattaglia@gmail.com>
2024-04-21 13:52:59 +0400

    finish

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,24 @@ export default function App({ Component, pageProps }) {
 
   return (
     <div className={`${raleway.variable} ${poppins.variable}`}>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '795110229198187');
+fbq('track', 'PageView');
+`,
+        }}
+      />
       <header
         className={`flex bg-primary sticky top-0 z-10 text-white   flex-col w-full `}
       >

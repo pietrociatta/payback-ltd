8ae5e2de6b1d95a2b1f5fa5cbe869af47e473ea3
pietrociatta <pietro.ciattaglia@gmail.com>
2024-05-17 10:51:51 +0200

    fixed day in today

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,10 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NQRLCBMH');`,
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PWCWG72P');`
           }}
         />
       </Head>
@@ -20,7 +20,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <NextScript />
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQRLCBMH" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWCWG72P"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
           }}
         />
       </body>
